25485d14c8d5372ff911a0589b2156b507d69e5d
Crowdbotics <team@crowdbotics.com>
2021-09-01 12:37:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wandering_lake_30246Identifier",
+  url: "https://wandering_lake_30246Identifier.botics.co",
+  api: "https://wandering_lake_30246Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
